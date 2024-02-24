409ecc501427db86f1385903b92e5b5010d03d29
Nigel <nigel.dm@telenet.be>
2024-02-24 16:14:53 +0100

    Nigel maakt een change... conflict ahead!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bye world!");
+        System.out.println("Bye world!(from Nigel)");
     }
 }
